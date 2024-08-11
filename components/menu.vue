62d8ffe6389a32fe6b596bdd7a3bb6dcafa3be8a
Sam Norris-Rogers <samnrogers@gmail.com>
2024-08-11 19:22:11 +0200

    setup 2

---
+++ components/menu.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <ul class="flex flex-col md:flex-row md:space-x-4">
+      <li>
+        <NuxtLink to="/" class="link">/home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about" class="link">/about</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/projects" class="link">/projects</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/blog" class="link">/blog</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/now" class="link">/now</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+.link {
+  @apply p-1 hover:text-gray-500 text-2xl md:text-base;
+  font-family: "JetBrains Mono";
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container mx-auto max-w-4xl">
+    <header class="flex justify-between items-start md:items-center mt-5">
+      <div class="flex items-center md:space-x-12">
+        <div class="hidden md:block">
+          <NuxtLink to="/" class="text-xl font-semibold p-2">
+            <h1>Sam Norris-Rogers</h1>
+          </NuxtLink>
+        </div>
+
+        <Menu />
+      </div>
+
+      <ClientOnly>
+        <ColorModeSelector />
+      </ClientOnly>
+    </header>
+
+    <main class="p-2 mt-10">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  titleTemplate: "%s - Sam Norris-Rogers",
+  link: [
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
+      crossorigin: "",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap",
+      crossorigin: "",
+    },
+  ],
+});
+</script>
+
+<style>
+body {
+  font-family: "Inter", sans-serif;
+}
+
+body {
+  @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300;
+}
+
+h1 {
+  font-family: "Inter";
+}
+</style>
